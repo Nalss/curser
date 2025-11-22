72bbbddde460b6b55b0432a1d005f758ef17d989
Kallss <kaarlokzop@gmail.com>
2025-11-22 21:16:56 +0100

    modified:   src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ package src;
 public class Main {
     public static void main(String[] args) {
         Gamepanel gamePanel = new Gamepanel(); // Create an instance of Gamepanel
-        gamePanel.openGUI(); // Call the method to launch the GUI
+        gamePanel.openGUI();                   // Launch the GUI
     }
  }
  
